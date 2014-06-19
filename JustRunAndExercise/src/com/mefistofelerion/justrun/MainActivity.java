466b4c9fa5caba9578dadfeb0a02b18542cd222a
Ivan Guerra <ivan.aguerra616@gmail.com>
2014-06-19 00:10:49 -0500

    se cambio README agregando los acentos donde se requerian, y se subio parte del proyecto inicial

---
+++ JustRunAndExercise/src/com/mefistofelerion/justrun/MainActivity.java
@@ -0,0 +1,80 @@
+package com.mefistofelerion.justrun;
+
+import android.app.Activity;
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.example.fatdecrease.R;
+
+public class MainActivity extends Activity {
+	
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+
+		if (savedInstanceState == null) {
+			getFragmentManager().beginTransaction()
+					.add(R.id.container, new PlaceholderFragment()).commit();
+		}
+		
+		
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		// Handle action bar item clicks here. The action bar will
+		// automatically handle clicks on the Home/Up button, so long
+		// as you specify a parent activity in AndroidManifest.xml.
+		int id = item.getItemId();
+		if (id == R.id.action_settings) {
+			return true;
+		}
+		return super.onOptionsItemSelected(item);
+	}
+
+	/**
+	 * A placeholder fragment containing a simple view.
+	 */
+	public static class PlaceholderFragment extends Fragment {
+
+		TextView t;
+		public PlaceholderFragment() {
+		}
+
+		@Override
+		public View onCreateView(LayoutInflater inflater, ViewGroup container,
+				Bundle savedInstanceState) {
+			View rootView = inflater.inflate(R.layout.fragment_main, container,
+					false);
+			t = new TextView(getActivity());
+			final Button button = (Button) rootView.findViewById(R.id.button_agree);
+			
+			t = (TextView) rootView.findViewById(R.id.welcome_text);
+			button.setOnClickListener(new View.OnClickListener() {
+				public void onClick(View v) {
+					t.setText("Welcome to my app");
+					button.setVisibility(TRIM_MEMORY_UI_HIDDEN);
+				}
+			});
+			return rootView;
+		}
+	}
+
+}
--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-Investigacion para tesis Agosto-Diciembre 2014 para la carrera Ingeniero en Tecnologia de Software de la Facultad de Ingenieria Mecanica y Electrica de la Universidad Autonoma de Nuevo Leon
+Investigación para tesis Agosto-Diciembre 2014 para la carrera Ingeniero en Tecnología de Software de la Facultad de Ingeniería Mecánica y Eléctrica de la Universidad Autónoma de Nuevo León
+
