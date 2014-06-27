2c32704ea60c05b4f0d5e67c9ba9e1e890252fc7
Ivan Guerra <ivan.aguerra616@gmail.com>
2014-06-26 21:42:01 -0500

    Se agrego funciones para detecciones de pasos y una funcion para verificar si existen los sensores que da soporte kitkat 4.4(solo nexus 5)

--- JustRunAndExercise/src/com/mefistofelerion/justrun/MainActivity.java
+++ JustRunAndExercise/src/com/mefistofelerion/justrun/MainActivity.java
@@ -1,8 +1,15 @@
 package com.mefistofelerion.justrun;
 
+import android.R.bool;
+import android.annotation.SuppressLint;
+import android.annotation.TargetApi;
 import android.app.Activity;
 import android.app.Fragment;
+import android.content.Intent;
+import android.content.pm.*;
+import android.os.Build;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -10,11 +17,9 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.TextView;
-
 import com.example.fatdecrease.R;
 
 public class MainActivity extends Activity {
-	
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -25,13 +30,10 @@ public class MainActivity extends Activity {
 			getFragmentManager().beginTransaction()
 					.add(R.id.container, new PlaceholderFragment()).commit();
 		}
-		
-		
 	}
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
-
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
@@ -51,10 +53,14 @@ public class MainActivity extends Activity {
 
 	/**
 	 * A placeholder fragment containing a simple view.
+	 * fragment for counting steps and possibly the welcome text
 	 */
 	public static class PlaceholderFragment extends Fragment {
 
-		TextView t;
+		private TextView t;
+		private boolean mIsRunning;
+		private int steps = 0;
+
 		public PlaceholderFragment() {
 		}
 
@@ -64,31 +70,62 @@ public class MainActivity extends Activity {
 			View rootView = inflater.inflate(R.layout.fragment_main, container,
 					false);
 			t = new TextView(getActivity());
-			final Button button = (Button) rootView.findViewById(R.id.button_agree);
-			
+			final Button button = (Button) rootView
+					.findViewById(R.id.button_agree);
+
 			t = (TextView) rootView.findViewById(R.id.welcome_text);
 			button.setOnClickListener(new View.OnClickListener() {
 				public void onClick(View v) {
-					t.setText("Bienvenido!, empecemos a jugar!");
+					t.setText("pasos dados: " + steps);
+					PackageManager pm = getActivity().getPackageManager();
+					t.setText("is valid: " + isKitKatWithStepCounter(pm) + " "
+							+ (int) Build.VERSION.SDK_INT);
+					// initPedometer();
 					button.setVisibility(TRIM_MEMORY_UI_HIDDEN);
 				}
 			});
 			return rootView;
 		}
-		
-		private void initAR(){
-			//config the video settings and initialize vuforia 
-			//initialize and start camera
-			
-	            
+
+		private void initAR() {
+			// config the video settings and initialize vuforia
+			// initialize and start camera
+
 		}
-		
-		private void initPedometer(){
-			//this will have the code for using the pedometer
+
+		private void startStepService() {
+			if (!mIsRunning) {
+				Log.i("JustRun", "[SERVICE] Start");
+				mIsRunning = true;
+				/*
+				 * startService(new Intent(PlaceholderFragment.this,
+				 * StepService.class));
+				 */
+			}
 		}
-		
-		private void saveInformation(){
-			//collect all the information for investigation purposes
+
+		// check if is has the needed sensors
+		@TargetApi(Build.VERSION_CODES.KITKAT)
+		@SuppressLint("InlinedApi")
+		public static Boolean isKitKatWithStepCounter(PackageManager pm) {
+
+			// Require at least Android KitKat
+			int currentApiVersion = (int) Build.VERSION.SDK_INT;
+			Log.i("Fragment", "android version: " + currentApiVersion);
+			// Check that the device supports the step counter and detector
+			// sensors
+
+			return currentApiVersion >= 19
+					&& pm.hasSystemFeature(android.content.pm.PackageManager.FEATURE_SENSOR_STEP_COUNTER)
+					&& pm.hasSystemFeature(android.content.pm.PackageManager.FEATURE_SENSOR_STEP_DETECTOR);
+		}
+
+		private void initPedometer() {
+			StepDetector detector = new StepDetector();
+		}
+
+		private void saveInformation() {
+			// collect all the information for investigation purposes
 		}
 	}
 
