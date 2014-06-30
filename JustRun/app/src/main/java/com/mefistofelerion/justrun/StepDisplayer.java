83dea49a29155941747a68193e6b8d8a885081bb
Ivan Guerra <ivan.aguerra616@gmail.com>
2014-06-29 23:50:44 -0500

    cambio de nombre en carpeta

---
+++ JustRun/app/src/main/java/com/mefistofelerion/justrun/StepDisplayer.java
@@ -0,0 +1,48 @@
+package com.mefistofelerion.justrun;
+
+import java.util.ArrayList;
+
+/**
+ * Created by ivan on 29/06/14.
+ */
+public class StepDisplayer implements StepListener {
+
+    private int count = 0;
+    private ArrayList<Listener> listeners = new ArrayList<Listener>();
+
+    public StepDisplayer() {
+
+    }
+
+    public void setSteps(int steps) {
+        this.count = steps;
+        notifyListener();
+    }
+
+    @Override
+    public void onStep() {
+        this.count++;
+        notifyListener();
+    }
+
+    @Override
+    public void passValue() {
+
+    }
+
+    public void addListener(Listener l) {
+        listeners.add(l);
+    }
+
+    private void notifyListener() {
+        for (Listener listener : listeners) {
+            listener.stepsChanged((int) count);
+        }
+    }
+
+    public interface Listener {
+        public void stepsChanged(int value);
+
+        public void passValue();
+    }
+}
---
+++ JustRun/app/src/main/java/com/mefistofelerion/justrun/StepListener.java
@@ -0,0 +1,10 @@
+package com.mefistofelerion.justrun;
+
+/**
+ * Created by ivan on 29/06/14.
+ */
+public interface StepListener {
+    public void onStep();
+
+    public void passValue();
+}
