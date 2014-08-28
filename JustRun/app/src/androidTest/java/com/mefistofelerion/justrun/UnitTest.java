6c2d34f6bef2847ce795afc07bbf569c97e4eab5
Ivan Guerra <ivan.aguerra616@gmail.com>
2014-08-27 22:34:10 -0500

    agregando esqueleto de unittesting y ver si es buena idea

---
+++ JustRun/app/src/androidTest/java/com/mefistofelerion/justrun/UnitTest.java
@@ -0,0 +1,15 @@
+package com.mefistofelerion.justrun;
+
+import android.test.ActivityUnitTestCase;
+
+/**
+ * using unit testing for sensors, lets see if it goes alright
+ * Created by ivan on 27/08/14.
+ */
+public class UnitTest extends ActivityUnitTestCase {
+
+    @Override
+    protected void setUp(){//here goes the unit testing
+     }
+
+}
