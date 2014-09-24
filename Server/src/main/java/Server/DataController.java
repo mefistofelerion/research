6009e6b3d8774635488d71c7ef6c56f2954bf138
Ivan Guerra <Ivan@Ivans-MacBook-Pro.local>
2014-09-24 12:09:15 -0500

    se agrego un nuevo esqueleto en el controller del server

--- Server/src/main/java/Server/DataController.java
+++ Server/src/main/java/Server/DataController.java
@@ -13,9 +13,37 @@ public class DataController {
 * obtiene los datos por medio de un request en formato json
 */
 	@ResponseBody
-    @RequestMapping(value = "/data", method={GET})
+    @RequestMapping(value = "/data", method={RequestMethod.GET})
     public ResponseEntity<String> dataFetching(@PathVariable Data data ) {
     	if(data!=null)
     		return new ResponseEntity<String>(HttpStatus.OK);
     	else
     		return new ResponseEntity<String>(HttpStatus.NOT_FOUND);
+
+/*
+* esta verifica si la conexion se encuentra bien
+*/
+
+	@ResponseBody
+	@RequestMapping(value="/data/works", method={RequestMethod.GET})
+	public ResponseEntity<Boolean> isWorking(){
+		if()//datos se pueden guardar o accesar
+			return true;
+		else{
+			return false;
+			}
+		}
+
+/*
+*puede regresar informacion mediante el id solicitado
+*/
+	@ResponseBody
+	@RequestMapping(value="/data/info/{id}", method={RequestMethod.GET}){
+		public ResponseEntity<Data> dataObtaining(@PathVariable String id){
+		//checar si hay conexion al database o archivo
+		//hacer un query con el id
+			Data info;//aqui pondre la información proveniente del query
+			return ResponseEntity<Data> info;
+		}
+	}
+}
