9954435fb1934814e2d9b1fd38ba7457ae1380b9
Ivan Guerra <Ivan@ivans-mbp.lan>
2014-10-06 23:58:43 -0500

    agregando repositorio para accesar ala base de datos con mongodb

--- Server/src/main/java/Server/DataRepository.java
+++ Server/src/main/java/Server/DataRepository.java
@@ -1,58 +1,8 @@
-import org.springframework.data.annotation.Id;
+import java.util.List;
 
+import org.springframework.data.mongodb.repository.MongoRepository;
 
-public class DataRepository {
+public interface DataRepository extends MongoRepository<DataEntity, String> {
 
-    private final int steps;
-    private final double distance;
-    private final double calories;
-
-    public DataRepository(){}
-
-    public DataRepository(int id, int steps, double distance, double calories) {
-        this.id = id;
-        this.steps = steps;
-        this.distance = distance;
-        this.calories = calories;
-    }
-
-    public int getId(){
-        return this.id;
-    }
-
-    public void setId(int id){
-        this.id = id
-    }
-
-    public int getSteps() {
-        return this.steps;
-    }
-
-    public void setSteps(int steps){
-        this.steps = steps;
-    }
-
-    public double getDistance() {
-        return this.distance;
-    }
-
-    public void setDistance(double distance){
-        this.distance = distance;
-    }
-
-    public double getCalories(){
-        return this.calories;
-    }
-
-    public void setCalories(double calories){
-        this.calories = calories
-    }
-
-    @Override
-    public String toString() {
-        return String.format(
-                "SensorData[id=%s, steps='%d', distance='%d', calories='%d']",
-                id, steps, distance,calories);
-    }
-
+    public DataEntity findById(String firstName);
 }
