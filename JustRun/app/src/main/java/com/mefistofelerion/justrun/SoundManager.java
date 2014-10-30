3ee978bcae44679a35e8ed2e96e2af522d3ee30e
Ivan Guerra <Ivan@Ivans-MacBook-Pro.local>
2014-10-29 22:55:47 -0600

    implementando soundmanager

--- JustRun/app/src/main/java/com/mefistofelerion/justrun/SoundManager.java
+++ JustRun/app/src/main/java/com/mefistofelerion/justrun/SoundManager.java
@@ -1,6 +1,7 @@
 package com.mefistofelerion.justrun;
 
 import android.content.Context;
+import android.media.AudioManager;
 import android.media.SoundPool;
 
 import java.util.ArrayList;
@@ -9,17 +10,30 @@ import java.util.ArrayList;
  * Created by Ivan on 10/28/14.
  */
 public class SoundManager {
+    // SoundPool is used for short sound effects
+    private SoundPool mSoundPool;
+
+    // An array of sound clip ids
+    public ArrayList<Integer> mSoundIds;
    
     /** Initialize the sound pool and load our sound clips. */
     public SoundManager(Context context)
     {
+        mSoundPool = new SoundPool(4, AudioManager.STREAM_MUSIC, 0);
+        mSoundIds = new ArrayList<Integer>();
 
+        mSoundIds.add(mSoundPool.load(context, R.shoot_sound, 1));
     }
 
 
     /** Play a sound with the given index and volume. */
     public void playSound(int soundIndex, float volume)
     {
+        if (soundIndex < 0 || soundIndex >= mSoundIds.size()) {
+            LoggerHelper.error("Sound id " + soundIndex + " out of range");
+            return;
+        }
+        mSoundPool.play(mSoundIds.get(soundIndex), volume, volume, 1, 0, 1.0f);
 
     }
 }
